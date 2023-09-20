5983ddf74d87185b935afd30385226b067f06a69
Andrew Healey <doolie.healey@gmail.com>
2023-09-20 00:18:35 +0000

    Move viz_dataset to parent module

--- autodistill_seggpt/__init__.py
+++ autodistill_seggpt/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.2"
 
-from .dataset_utils import label_dataset, shrink_dataset_to_size
+from .dataset_utils import label_dataset, shrink_dataset_to_size,viz_dataset
 from .few_shot_ontology import FewShotOntology
 
 from .find_examples.sample import sample_ontology
